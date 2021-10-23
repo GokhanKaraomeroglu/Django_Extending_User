badae940cf1f4b77ee6afe02c5de8757762588da
Himmel König <itogreniyorum@gmail.com>
2021-10-23 16:27:06 +0300

    bug fixed

--- users/views.py
+++ users/views.py
@@ -26,3 +26,20 @@ def register(request):
     }
 
     return render(request, "users/register.html", context)
+
+def user_logout(request):
+    messages.success(request, "You Logout!")
+    logout(request)
+    return redirect('home')
+
+def user_login(request):
+
+    form = AuthenticationForm(request, data=request.POST)
+
+    if form.is_valid():
+        user = form.get_user()
+        if user:
+            messages.success(request, "Login successfull")
+            login(request, user)
+            return redirect('home')
+    return render(request, 'users/user_login.html', {"form": form})
